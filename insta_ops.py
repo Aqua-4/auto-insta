4293818aca7848e4810396a14db6e674e71c27e4
Parashar Sangle <parashar.sangle@gramener.com>
2019-04-20 02:00:50 +0530

    WIP:login & ADD get_usr_name & FIX chromedriver exec path

--- insta_ops.py
+++ insta_ops.py
@@ -15,13 +15,15 @@ import random
 import sqlite3
 import time
 import pyttsx3
+import platform
 
 
 class InstaOps:
 
     def __init__(self):
         self.db_conn = sqlite3.connect('auto-insta.db')
-        self.user_id = pd.read_sql("select user_id from creds", self.db_conn).user_id[0]
+        self.user_id = pd.read_sql(
+            "select user_id from creds", self.db_conn).user_id[0]
 
         options = webdriver.ChromeOptions()
         chromedata = pd.read_sql(
@@ -29,8 +31,13 @@ class InstaOps:
         if chromedata:
             options.add_argument(chromedata)
         options.add_argument("--start-maximized")
+
+        exec_path = "chromedriver"
+        if platform.system() == 'Linux':
+            exec_path = "./chromedriver"
+
         self.driver = webdriver.Chrome(
-            executable_path="chromedriver", chrome_options=options)
+            executable_path=exec_path, chrome_options=options)
         self.engine = pyttsx3.init()
 
     def insta_login(self):
@@ -40,21 +47,21 @@ class InstaOps:
 
         if self.driver.current_url != "https://www.instagram.com/":
             time.sleep(5)
-            user_name = self.driver.find_element_by_xpath(
+            user_id = self.driver.find_element_by_xpath(
                 '//input[@name="username"]')
-            user_name.clear()
-            user_name.send_keys(self.user_id)
+            user_id.clear()
+            user_id.send_keys(self.user_id)
             passwd = self.driver.find_element_by_xpath(
                 '//input[@name="password"]')
             passwd.clear()
-            passwd.send_keys(pd.read_sql("select passwd from creds", self.db_conn).passwd[0])
+            passwd.send_keys(pd.read_sql(
+                "select passwd from creds", self.db_conn).passwd[0])
             passwd.send_keys(Keys.RETURN)
             time.sleep(10)
 
-            self.driver.get("https://www.instagram.com/{}/".format(self.user_id))
-
+        self.user_name = self.__get_usr_name()
         login_greet = "Welcome {}, you have been logged into your account and ".format(
-            self.USER_NAME)
+            self.user_name)
         login_greet += "here are your stats for the current session "
         self.text_to_speech(login_greet, False)
         self.FOLLOWER_CNT = self.get_number_of(
@@ -63,10 +70,27 @@ class InstaOps:
             "https://www.instagram.com/{}/following/".format(self.user_id))
         self.compare_stats(self.FOLLOWER_CNT, self.FOLLOWING_CNT, True)
 
+# --------------____________________________Private Func_________________________-------------------
+
+    def __get_usr_name(self):
+        # update/get user_name from insta/db
+
+        self.driver.get("https://www.instagram.com/{}/".format(self.user_id))
+        usr_name = pd.read_sql(
+            "select user_name from creds", self.db_conn).user_name[0]
+        if usr_name:
+            return usr_name
+        else:
+            usr_name = self.driver.find_elements_by_tag_name('h1')[1].text
+            self.db_conn.execute('''UPDATE creds SET user_name="{}"
+             where user_id="{}"'''.format(usr_name, self.user_id))
+            return usr_name
+
+
 # --------------______________________________Utils______________________________-------------------
 
-
     def text_to_speech(self, text_string="Test", bool_print=True, bool_speak=True):
+        # setup TTS for audio output
         if bool_print:
             print(text_string)
         if bool_speak:
