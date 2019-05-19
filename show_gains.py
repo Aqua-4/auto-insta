63a530ae0efe757d6d9556ae8ba3e204a5e481e5
Aqua-4 <punks23not8dead@gmail.com>
2019-05-19 23:31:15 +0530

    INIT bot stats info

---
+++ show_gains.py
@@ -0,0 +1,18 @@
+"""
+Show following Insights:
+1. followers gained
+2. algorithm accuracy
+"""
+
+from insta_ops import InstaOps
+import pandas as pd
+from random import randint, choice
+import time
+
+
+bot = InstaOps(False)
+bot.account_init()
+
+#bot.sync_db()
+bot.refresh_db()
+
