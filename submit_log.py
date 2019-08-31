6be6d57cd5786ed6420b75946a583fac28324c80
Parashar Sangle <parashar.sangle@gramener.com>
2019-08-31 18:41:28 +0530

    WIP: Module to create log and send

---
+++ submit_log.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 31 18:18:24 2019
+
+@author: parashar
+"""
+
+import sqlite3
+import pandas as pd
+import shutil
+
+
+db_conn = sqlite3.connect('auto-insta (copy).db')
+
+usr = pd.read_sql("select user_id from creds",con=db_conn).user_id[0]
+db_copy = '{}_db.db'.format(usr)
+shutil.copy('auto-insta.db',db_copy)
+
+db_conn = sqlite3.connect(db_copy)
+db_conn.execute("DROP TABLE creds")
+print("Successfully removed all the Login detials")
+db_conn.commit()
+
+print("File *auto_insta.log* and *{}* can now be sent to the dev".format(db_copy))
