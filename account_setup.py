7bf79f0bd81f849d4b39ac0d3ddfcfb809f4c5a1
Parashar Sangle <parashar.sangle@gramener.com>
2019-09-01 01:54:11 +0530

    ADD how_to str

--- account_setup.py
+++ account_setup.py
@@ -15,7 +15,17 @@ conn.execute('''INSERT INTO creds (user_id,passwd)
 
 print("credentials have been udpated")
 print('Do you want to update "Chrome Data Location"')
+
+how_to = """
+*****How to Find Path Location:*******
+1. Open chrome
+2. open Link/URL "chrome://version/"
+3. copy "Profile Path"
+4. paste here
+"""
+
 if input("press y or n:-").lower() == 'y':
+    print(how_to)
     path_loc = input("Path Location:")
     conn.execute('UPDATE creds SET chromedata="{}" where user_id="{}"'.format(
         path_loc.replace("/", "//"), user_id))
