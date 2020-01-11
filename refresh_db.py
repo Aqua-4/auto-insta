371a1660ff53b5bafee8c47b075c79773c486ad2
Aqua-4 <punks23not8dead@gmail.com>
2020-01-11 23:44:10 +0530

    ADD: multi-processing to update missing_user_meta

--- refresh_db.py
+++ refresh_db.py
@@ -7,6 +7,21 @@ Created on Sat Aug 31 18:44:46 2019
 """
 
 from insta_ops import InstaOps
+from random import randint, choice
+import time
+import multiprocessing as mp
+
+
+def worker(user):
+    instance = InstaOps(True, True, True, True)
+
+    try:
+        meta = instance._user_meta(user)
+        instance._update_meta(user, meta, True)
+    except:
+        instance.db_conn.execute('''UPDATE instaDB
+            SET acc_status=0 WHERE user_id="{usr}";
+        '''.format(usr=user))
 
 
 bot = InstaOps(False)
@@ -16,5 +31,12 @@ bot.sync_db()
 bot.refresh_db()
 bot.__del__()
 
-incognito = InstaOps(False, True, True)
-incognito.refresh_db()
+incognito = InstaOps(True, True, True, True)
+missing_users = incognito._get_missing_meta_users()
+
+print("Commencing HAVOC, please close other applications...")
+if __name__ == '__main__':
+    p = mp.Pool(mp.cpu_count())
+    p.map(worker, missing_users)
+
+
